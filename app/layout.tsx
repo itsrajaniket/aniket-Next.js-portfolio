b8bea2f97ee831bcc109cd1df58e587d28aa3926
itsrajaniket <itsrajaniket@gmail.com>
2026-04-14 00:16:03 +0530

    feat: aded microsoft clarity code to get info about user visit

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,18 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           crossOrigin="anonymous"
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wb6izc7ndb");
+            `,
+          }}
+        />
       </head>
       <body className="bg-base text-main font-sans antialiased transition-colors duration-500">
         <ClientLayoutProviders>
